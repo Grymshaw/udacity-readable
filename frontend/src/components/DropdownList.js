ab4beb606c096e8a61a155cf6415cfb4df1f4ae9
Kyle Grymonpre <kgrymonpr@gmail.com>
2017-09-27 22:42:59 -0500

    feat: Add DropdownList component and tests

---
+++ frontend/src/components/DropdownList.js
@@ -0,0 +1,30 @@
+/* eslint "react/jsx-filename-extension": 0 */
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+class DropdownList extends Component {
+  componentDidMount() {
+    this.props.fetchData();
+  }
+
+  render() {
+    const { list, onChange } = this.props;
+    return (
+      <select onChange={onChange}>
+        {list && list.length
+          ? list.map(item => (
+            <option key={item}>{item}</option>
+          ))
+          : null}
+      </select>
+    );
+  }
+}
+
+DropdownList.propTypes = {
+  fetchData: PropTypes.func.isRequired,
+  list: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default DropdownList;
---
+++ frontend/src/components/DropdownList.spec.js
@@ -0,0 +1,56 @@
+/* eslint "no-undef": 0 */
+/* eslint "react/jsx-filename-extension": 0 */
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import React from 'react';
+import sinon from 'sinon';
+
+import DropdownList from './DropdownList';
+
+describe('<DropdownList />', () => {
+  let fetchData;
+  let onChange;
+  let wrapper;
+  beforeEach(() => {
+    onChange = sinon.spy();
+    fetchData = sinon.spy();
+    wrapper = shallow(
+      <DropdownList
+        list={['react', 'udacity']}
+        onChange={onChange}
+        fetchData={fetchData}
+      />);
+  });
+
+  it('renders a <select>', () => {
+    expect(wrapper.find('select').length).to.be.above(0);
+  });
+
+  it('renders a select with no options if `list` is not passed', () => {
+    wrapper = shallow(<DropdownList
+      list={[]}
+      onChange={onChange}
+      fetchData={fetchData}
+    />);
+    expect(wrapper.find('option').length).to.equal(0);
+  });
+
+  it('renders a selcted with no options if `list` is []', () => {
+    wrapper = shallow(<DropdownList
+      list={[]}
+      onChange={onChange}
+      fetchData={fetchData}
+    />);
+    expect(wrapper.find('option').length).to.equal(0);
+  });
+
+  it('renders an `option` for each item in `list` param', () => {
+    expect(wrapper.find('option').length).to.equal(2);
+  });
+
+  it('calls `onChange` prop when new value selected', () => {
+    wrapper.simulate('change', 'udacity');
+    expect(onChange.callCount).to.equal(1);
+    expect(onChange.calledWith('udacity')).to.equal(true);
+  });
+});
