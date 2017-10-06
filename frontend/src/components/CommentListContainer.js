6f5b13685c73e478500436d67215cdc8bc14bac9
Kyle Grymonpre <kgrymonpr@gmail.com>
2017-10-05 21:04:51 -0500

    feat: Add sorting for comments

--- frontend/src/components/CommentListContainer.js
+++ frontend/src/components/CommentListContainer.js
@@ -1,14 +1,32 @@
 import { connect } from 'react-redux';
 
-import * as actions from '../actions/comments';
 import CommentList from './CommentList';
+import * as postActions from '../actions/comments';
+// import * as sortActions from '../actions/sortOrder';
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  onMount: () => actions.fetchPostComments(ownProps.parentId)(dispatch),
+  onMount: () => {
+    postActions.fetchPostComments(ownProps.parentId)(dispatch);
+    // dispatch(sortActions.setSortOrder('recentFirst'));
+  },
 });
 
 const mapStateToProps = state => ({
-  comments: Object.keys(state.comments.comments).map(key => state.comments.comments[key]),
+  comments: Object.keys(state.comments.comments)
+    .map(key => state.comments.comments[key])
+    .sort((a, b) => {
+      switch (state.sortOrder.order) {
+        case 'recentFirst':
+          return b.timestamp - a.timestamp;
+        case 'oldestFirst':
+          return a.timestamp - b.timestamp;
+        case 'votesAscending':
+          return a.voteScore - b.voteScore;
+        case 'votesDescending':
+        default:
+          return b.voteScore - a.voteScore;
+      }
+    }),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CommentList);
