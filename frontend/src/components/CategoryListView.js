7017a8735f7cb3ab4e0efe281038e9c915a52e9d
Kyle Grymonpre <kgrymonpr@gmail.com>
2017-10-14 13:09:07 -0500

    feat: Add key PostListContainer to make it rerender when switching categories

--- frontend/src/components/CategoryListView.js
+++ frontend/src/components/CategoryListView.js
@@ -1,27 +1,16 @@
 /* eslint "react/jsx-filename-extension": 0 */
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import AddPostButton from './AddPostButton';
-import CategoryList from './CategoryList';
+import Navigation from './Navigation';
 import PostListContainer from './PostListContainer';
-import PrimaryNav from './PrimaryNav';
-import SortOrderList from './SortOrderList';
-import Subnav from './Subnav';
 
 const CategoryListView = ({ match }) => (
   <div>
-    <PrimaryNav>
-      <h3><Link to="/">Readable</Link></h3>
-    </PrimaryNav>
-    <Subnav>
-      <AddPostButton>Add post +</AddPostButton>
-      <div>
-        <SortOrderList />
-        <CategoryList category={match.params.category} />
-      </div>
-    </Subnav>
-    <PostListContainer category={match.params.category} />
+    <Navigation match={match} />
+    <div className="page">
+      {/* add key property to force re-render when category param in url is different */}
+      <PostListContainer key={match.params.category} category={match.params.category} />
+    </div>
   </div>
 );
 
